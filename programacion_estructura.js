722e8bf954856f8e18e542881b032824e14ee87b
GeraldineTequia <tequiageraldine7@gmail.com>
2020-08-21 10:12:24 -0500

    programacion poo

---
+++ programacion_estructura.js
@@ -0,0 +1,21 @@
+
+
+let  nombre  =  'Geraldine' ;
+let  apellido  =  'Tequia' ;
+let  edad  =  16 ;
+
+console.log ( `hola mi nombre es: ${nombre}  ${apellido} y tengo ${edad} años` ) ; 
+
+
+
+ let num = 20 ;
+ function multiplicar (valor) {
+    return valor * valor ;
+}
+
+let  resultado =  multiplicar ( 5 ) * Num ;
+console. log ( resultado )
+let nombre = 'Derly';
+let apellido = 'Murcia';
+let edad = 23;
+
