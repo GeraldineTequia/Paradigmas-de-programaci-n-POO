2b9f1eb8852145187bc01ff27cf7ae7c66599e48
Geraldine Tequia <66040433+GeraldineTequia@users.noreply.github.com>
2020-08-23 18:00:17 -0500

    Update programacion_estructura.js

--- programacion_estructura.js
+++ programacion_estructura.js
@@ -13,9 +13,6 @@ console.log ( `hola mi nombre es: ${nombre}  ${apellido} y tengo ${edad} años` 
     return valor * valor ;
 }
 
-let  resultado =  multiplicar ( 5 ) * Num ;
-console. log ( resultado )
-let nombre = 'Derly';
-let apellido = 'Murcia';
-let edad = 23;
+
+
 
